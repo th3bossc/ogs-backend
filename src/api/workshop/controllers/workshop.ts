69d23cd91d5a4beaef69d2a4a1e943963ccf9fbb
th3bossc <diljith2003@gmail.com>
2025-02-18 20:15:55 +0530

    modified worksohp controller

--- src/api/workshop/controllers/workshop.ts
+++ src/api/workshop/controllers/workshop.ts
@@ -4,4 +4,24 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::workshop.workshop');
+export default factories.createCoreController('api::workshop.workshop', ({ strapi }) => ({
+    async find(ctx) {
+        const query = {
+            ...ctx.query,
+            populate: ['image', 'venue']
+        }
+
+        ctx.query = query;
+        return super.find(ctx);
+    },
+
+    async findOne(ctx) {
+        const query = {
+            ...ctx.query,
+            populate: ['image', 'venue']
+        }
+
+        ctx.query = query;
+        return super.findOne(ctx);
+    },
+}));
