4a7e64d78d1002bfbeb1bea633f6509f5d2e1d7a
th3bossc <diljith2003@gmail.com>
2025-02-18 17:42:16 +0530

    added auto population config

--- src/api/venue/controllers/venue.ts
+++ src/api/venue/controllers/venue.ts
@@ -4,4 +4,24 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::venue.venue');
+export default factories.createCoreController('api::venue.venue', ({ strapi }) => ({
+    async find(ctx) {
+        const query = {
+            ...ctx.query,
+            populate: ['image', 'events']
+        }
+
+        ctx.query = query;
+        return super.find(ctx);
+    },
+
+    async findOne(ctx) {
+        const query = {
+            ...ctx.query,
+            populate: ['image', 'events']
+        }
+
+        ctx.query = query;
+        return super.findOne(ctx);
+    }
+}));
